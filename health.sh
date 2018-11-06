778aa1f71bfcd94b149983373bc10ecf6a652887
Jiwon Kang <33274408+rkd8923@users.noreply.github.com>
2018-11-07 02:23:48 +0900

    CPU/DISK/TRAFFIC clear

--- health.sh
+++ health.sh
@@ -2,24 +2,27 @@
 for((;;))
 do 
   CPU=`top -n 1 | grep -i cpu\(s\)| awk '{print $5}' | tr -d "%id," | awk '{print 100-$1}'`
-  echo "$CPU"
-:<<END
-  for t in $CPU2
-  do
-    if [ ${t} != "%Cpu(s):" -a ${t} != "us," -a ${t} != "ni," -a ${t} != "sy," -a ${t} != "id," -a ${t} != "wa," -a ${t} != "hi," -a ${t} != "si," -a ${t} != "st" ];then
-    echo "$t"
-    #CPU_LOAD="$CPU_LOAD+$t" | bc -l
-    CPU_LOAD=$(echo "$CPU_LOAD" "$t" | awk '{ printf "%f", $1 + $2 }') 
-    echo "$CPU_LOAD"
-    fi
-  done
-END
+  echo "CPU $CPU %"
   DISK_TOTAL=`df -P | grep -v ^Filesystem | awk '{sum += $2} END { print sum; }'`
   DISK_USED=`df -P | grep -v ^Filesystem | awk '{sum += $3} END { print sum; }'`
   DISK_PER=`echo "100*$DISK_USED/$DISK_TOTAL" | bc -l`
-  echo "$DISK_PER %"
-  ifconfig -s
-
-  sleep 5
+  echo "DISK $DISK_PER %"
+  T=`ifconfig -s`
+  RX=0
+  TX=0
+  i=0
+  for x in $T;
+  do
+    if [ `expr $i % 11` -eq 2 ];then
+      RX=$(($RX+$x))
+    fi
+    if [ `expr $i % 11` -eq 6 ];then
+      TX=$(($TX+$x))
+    fi
+    i=$(($i+1))
+  done
+  echo "RX $RX"
+  echo "TX $TX"
+  sleep 3
 done
   
